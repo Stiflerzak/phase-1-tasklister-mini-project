17325d733046ed078bfc518ea9865783af4ac4f9
Stiflerzak <hilarymwendia97@gmail.com>
2022-12-09 23:45:32 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let todo = document.querySelector('form')
+    
+    todo.addEventListener('submit', (e) => {
+      e.preventDefault()
+      addTask(e.target.task.value);
+
+    })
+  });
+
+  function addTask(e){
+    let p = document.createElement('p')
+    p.textContent = e
+    document.querySelector('#main-content').appendChild(p)
+
+
+  }
